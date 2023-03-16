7cdb39837ce6aac5f5c5a4dea3b1cc431e7afc9c
Peter Bared <peter.bared@sunne.se>
2023-03-16 11:37:13 +0100

    test

--- loopar/javascript.js
+++ loopar/javascript.js
@@ -1,7 +1,7 @@
 let main = document.querySelector("main");
 let cards = "";
 for (let i = 1; i < 26; i++) {
-    cards += `<img src="Samples/LandscapeImage${i}.jpg">`;
+    cards += `<div><img src="Samples/LandscapeImage${i}.jpg"></div>`;
 }
 console.log(cards);
 main.innerHTML = cards;
